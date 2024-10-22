f2024e66fe1597837e29d9005d9e89bae0ca04b9
Aleskei Budkin <anibaka9@gmail.com>
2024-10-22 17:28:32 +0600

    added speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import { Bricolage_Grotesque } from "next/font/google";
 import { Syne } from "next/font/google";
 
@@ -37,6 +39,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
